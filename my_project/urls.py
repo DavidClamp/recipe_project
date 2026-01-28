238b839dc5d54818f4bb5f1edc42fed7fd35d196
David Clamp <dclamp@yahoo.com>
2026-01-28 17:14:13 +0000

    Create generic django view and test deployment (debug=False)

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from center import views as index_views
+# from center import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("center.urls"), name="center-urls"),
+    # path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
