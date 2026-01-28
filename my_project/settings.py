53778bb22d8fda20b338d1da8879e03c59a53b84
David Clamp <dclamp@yahoo.com>
2026-01-28 10:10:58 +0000

    Deploy Progres database and secret key

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#6_c9vae-y#s1=$12v3su*jv0mv$16p30sw$hpdlfx@83#s9)*'
+SECRET_KEY = os.environ.get('Fumble%123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
