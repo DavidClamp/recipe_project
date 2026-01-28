d07bc5402e5baee684a0092254d9acdf41dba066
David Clamp <dclamp@yahoo.com>
2026-01-28 13:40:34 +0000

    Ran Django server and logged in as superuser inadmin

--- center/models.py
+++ center/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Finished"))
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    # state if recipe is still in draft of finished(published)
+
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # show title as a readable string 
+
+    def __str__(self):
+        return self.title
+    
+
--- my_project/settings.py
+++ my_project/settings.py
@@ -24,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('Fumble%123')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',
+                 'localhost',]
 
 
 # Application definition
@@ -87,7 +89,11 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
+# add trusted origins for local dev server URL domain and production server URL heroku
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
