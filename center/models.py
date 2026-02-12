73ada9a6bd2d48a0e26727cff6d7e197b19ebeca
David Clamp <dclamp@yahoo.com>
2026-02-12 08:29:37 +0000

    debug clean whitespace

--- center/models.py
+++ center/models.py
@@ -3,28 +3,24 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Finished"))
 
-# Create your models here.
 class Recipe(models.Model):
-
+    """
+    Model representing a recipe post.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts"
+    )
     description = models.TextField(blank=True, null=True)
     ingredients = models.TextField()
     instructions = models.TextField()
-
-    # state if recipe is still in draft of finished(published)
-
     status = models.IntegerField(choices=STATUS, default=0)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-# order recipes with updates first then second created
 
     class Meta:
         ordering = ["-updated_at", "-created_at"]
 
-    # show title as a readable string
     def __str__(self):
         return f"The title of this recipe is {self.title}"
