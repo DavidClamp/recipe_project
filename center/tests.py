31ac5906348c9664764f0e0f3add924b3a8b2b61
David Clamp <dclamp@yahoo.com>
2026-03-30 17:51:28 +0100

    Update TESTING.md with tests.py validation

--- center/tests.py
+++ center/tests.py
@@ -8,7 +8,8 @@ class TestAddRecipeView(TestCase):
     """Verify functionality and security of the recipe creation view."""
 
     def setUp(self):
-        self.user = User.objects.create_user(username='chef_david', password='password123')
+        self.user = User.objects.create_user
+        (username='chef_david', password='password123')
         self.client.login(username='chef_david', password='password123')
         self.url = reverse('add_recipe')
 
@@ -25,7 +26,7 @@ class TestAddRecipeView(TestCase):
             'status': 1
         }
         response = self.client.post(self.url, recipe_data)
-        self.assertEqual(response.status_code, 302) 
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(Recipe.objects.count(), 1)
 
     def test_unauthenticated_user_redirect(self):
@@ -39,25 +40,29 @@ class TestEditDeleteViews(TestCase):
     """Verify permission-based CRUD operations for recipe authors."""
 
     def setUp(self):
-        self.author = User.objects.create_user(username='chef_david', password='password123')
-        self.malicious_user = User.objects.create_user(username='bad_actor', password='password123')
+        self.author = User.objects.create_user
+        (username='chef_david', password='password123')
+        self.malicious_user = User.objects.create_user
+        (username='bad_actor', password='password123')
         self.recipe = Recipe.objects.create(
-            title='Old Stew', 
-            slug='old-stew', 
-            author=self.author, 
-            ingredients='Beef', 
+            title='Old Stew',
+            slug='old-stew',
+            author=self.author,
+            ingredients='Beef',
             instructions='Cook it.'
         )
 
     def test_author_can_access_edit_page(self):
         self.client.login(username='chef_david', password='password123')
-        response = self.client.get(reverse('edit_recipe', args=[self.recipe.slug]))
+        response = self.client.get
+        (reverse('edit_recipe', args=[self.recipe.slug]))
         self.assertEqual(response.status_code, 200)
 
     def test_malicious_user_access_denied(self):
         """Confirm a non-author is blocked from accessing the edit route."""
         self.client.login(username='bad_actor', password='password123')
-        response = self.client.get(reverse('edit_recipe', args=[self.recipe.slug]))
+        response = self.client.get
+        (reverse('edit_recipe', args=[self.recipe.slug]))
         self.assertNotEqual(response.status_code, 200)
 
     def test_successful_recipe_update(self):
@@ -69,7 +74,8 @@ class TestEditDeleteViews(TestCase):
             'instructions': 'Simmer for 2 hours.',
             'status': 1
         }
-        response = self.client.post(reverse('edit_recipe', args=[self.recipe.slug]), updated_data)
+        response = self.client.post
+        (reverse('edit_recipe',  args=[self.recipe.slug]), updated_data)
         self.assertEqual(response.status_code, 302)
         self.recipe.refresh_from_db()
         self.assertEqual(self.recipe.title, 'New Stew')
